2f3ac4d3f2fc81bc729422a86448da561e36c2ac
smdurjoy <smdurjoy963@gmail.com>
2021-03-14 23:09:16 +0600

    fetch countries

---
+++ components/CountryTable/CountryTable.js
@@ -0,0 +1,28 @@
+import styles from './CountryTable.module.css'
+
+const CountriesTable = ({ countries }) => {
+    return(
+        <div>
+            <div className={styles.heading}>
+                <button className={styles.heading_name}>
+                    <div>Name</div>
+                </button>
+
+                <button className={styles.heading_population}>
+                    <div>Population</div>
+                </button>
+            </div>
+
+            {
+                countries.map((country) => (
+                    <div className={styles.row} key={country.id}>
+                        <div className={styles.name}>{country.name}</div>
+                        <div className={styles.population}>{country.population}</div>
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default CountriesTable
